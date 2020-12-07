5bdb5f1019515764875afead5c37944cc824da93
Abdulloh Musayev <musayevabdulloh76@gmail.com>
2020-12-07 06:01:41 +0500

    docs: add config files for some dependencies

---
+++ rs-css/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+};
